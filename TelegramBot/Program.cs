ff96ae9b8999010cb58712ed91f92ca2e62ddaa0
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-17 14:09:36 +0300

    Add token and get json method

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace TelegramBot
+﻿using Newtonsoft.Json.Linq;
+
+namespace TelegramBot
 {
 	internal class Program
 	{
+		static string Token = GetJson("config.json");
 		static void Main(string[] args)
 		{
-			
+            Console.WriteLine(Token);
+        }
+
+		static string GetJson(string filePath)
+		{
+			string key = string.Empty;
+
+			try
+			{
+				string jsonString = File.ReadAllText(filePath);
+				JObject json = JObject.Parse(jsonString);
+				key = (string)json["key"]!;
+				return key;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Помилка при зчитуванні JSON файлу: {ex.Message}");
+				return "";
+			}
+
 		}
 	}
 }
