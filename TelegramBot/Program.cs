49d7567918af8a072dca7fb8d36b7f0580136988
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-17 15:18:45 +0300

    Added simple logic for answers

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,22 +1,58 @@
 ﻿using Newtonsoft.Json.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TelegramBot
 {
 	internal class Program
 	{
 		static string Token = GetJson("config.json");
+		static TelegramBotClient botClient = new TelegramBotClient(Token);
 		static void Main(string[] args)
 		{
-            Console.WriteLine(Token);
+            CancellationTokenSource source = new CancellationTokenSource();
+			CancellationToken cancellation = source.Token;
+
+			ReceiverOptions options = new ReceiverOptions()
+			{
+				AllowedUpdates = {},
+			};
+
+			botClient.StartReceiving(BotTakeMessage, BotTakeError, options, cancellation);
+
+			Console.ReadKey();
         }
 
+		static async Task BotTakeMessage(ITelegramBotClient botClient, Update update, CancellationToken token)
+		{
+			if(update.Type == UpdateType.Message)
+			{
+				Message message = update.Message;
+				if (message.Type == MessageType.Text)
+				{
+					await botClient.SendTextMessageAsync(message.Chat.Id, $"Привіт, {message.Chat.FirstName}. Я працюю!");
+                    await Console.Out.WriteLineAsync(message.Chat.FirstName + ": " + message.Text);
+                }
+			
+			}
+			
+		}
+
+		static async Task BotTakeError(ITelegramBotClient botClient, Exception ex, CancellationToken token)
+		{
+
+		}
+
+		#region Get Json
 		static string GetJson(string filePath)
 		{
 			string key = string.Empty;
 
 			try
 			{
-				string jsonString = File.ReadAllText(filePath);
+				string jsonString = System.IO.File.ReadAllText(filePath);
 				JObject json = JObject.Parse(jsonString);
 				key = (string)json["key"]!;
 				return key;
@@ -28,5 +64,6 @@ namespace TelegramBot
 			}
 
 		}
+		#endregion
 	}
 }
