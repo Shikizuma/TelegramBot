c5221923f0c5b337116fcea9b2556c3bace4bf83
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-18 18:06:40 +0300

    Add logic with rate and views

--- TelegramBot/MovieBot.cs
+++ TelegramBot/MovieBot.cs
@@ -51,55 +51,65 @@ namespace TelegramBot
 
 				if(message.Type == MessageType.Text)
 				{
-					if(message.Text == "Рандомний фільм")
-					{
-						var film = GetRandomFilm();
-						
-						await ShowFilm(message.Chat.Id, film);
-						return;
-					}
-
-					if (message.Text == "Топ фільмів")
-					{
-						var films = GetTopFilms(5);
-						foreach(var film in films)
-						{
-							await ShowFilm(message.Chat.Id, film);
-						}
-						return;
-					}
-
-					if (message.Text == "Знайти фільм")
-					{
-						await botClient.SendTextMessageAsync(message.Chat.Id, "Оберіть тип пошуку", replyMarkup: MarkupMenu.SearchMenu);
-
-						if(message.Text == "За жанром")
-						{
-
-						}
-						if(message.Text == "За назвою")
-						{
-
-						}
-						if(message.Text == "Назад у меню")
-						{
-							await botClient.SendTextMessageAsync(message.Chat.Id, "Ви повернулись у головне меню", replyMarkup: MarkupMenu.MainMenu);
-						}
-						return;
-					}
-
-					string responce = GetResponce(message.Text);
-					await botClient.SendTextMessageAsync(message.Chat.Id, responce, replyMarkup: MarkupMenu.MainMenu);
-					await Console.Out.WriteLineAsync(message.Chat.FirstName + ": " + message.Text);
+					GetTextMessage(message);
 				}		
 			}
 		}
-		
+
+		public async Task GetTextMessage(Message message)
+		{
+			if (message.Text == "Рандомний фільм")
+			{
+				var film = GetRandomFilm();
+
+				await ShowFilm(message.Chat.Id, film);
+				return;
+			}
+
+			if (message.Text == "Топ фільмів")
+			{
+				var films = GetTopFilms(5);
+				foreach (var film in films)
+				{
+					await ShowFilm(message.Chat.Id, film);
+				}
+				return;
+			}
+
+			if (message.Text == "Знайти фільм")
+			{
+				await botClient.SendTextMessageAsync(message.Chat.Id, "Оберіть тип пошуку", replyMarkup: MarkupMenu.SearchMenu);
+
+				if (message.Text == "За жанром")
+				{
+
+				}
+				if (message.Text == "За назвою")
+				{
+
+				}
+				if (message.Text == "Назад у меню")
+				{
+					await botClient.SendTextMessageAsync(message.Chat.Id, "Ви повернулись у головне меню", replyMarkup: MarkupMenu.MainMenu);
+				}
+				return;
+			}
+
+			string responce = GetResponce(message.Text);
+			await botClient.SendTextMessageAsync(message.Chat.Id, responce, replyMarkup: MarkupMenu.MainMenu);
+			await Console.Out.WriteLineAsync(message.Chat.FirstName + ": " + message.Text);
+		}
+
+
 		public async Task ShowFilm(long chat, FilmModel film)
 		{
 			var buttons = InlineMenu.SetRate(film);
-			await botClient.SendPhotoAsync(chatId: chat, photo: film.Image);
-			await botClient.SendTextMessageAsync(chatId: chat, film.Name +  "\n" + film.Description + "\n" + "Рейтинг: " + film.Rate, replyMarkup: buttons);
+			await botClient.SendPhotoAsync(
+				chatId: chat, 
+				photo: film.Image, 
+				caption: film.Name + "\n" + film.Description + "\n" + "Рейтинг: " + film.Rate, 
+				replyMarkup: buttons);
+			//await botClient.SendTextMessageAsync(chatId: chat, );
 
 		}
 
