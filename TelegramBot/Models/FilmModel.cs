b63c12ae1da72f4f436eb3b74327913aaea41f3e
Miroslav <myroslav.bashkirov@gmail.com>
2023-09-02 17:17:31 +0300

    Add some changes

--- TelegramBot/Models/FilmModel.cs
+++ TelegramBot/Models/FilmModel.cs
@@ -9,11 +9,14 @@ namespace TelegramBot.Models
 {
 	internal class FilmModel
 	{
-
         public string Name { get; set; }
 		public int ReleaseYear { get; set; }
         public string Description { get; set; }
-		public string Genre { get; set; }
+        public string Country { get; set; }
+        public Dictionary<string, string> Director { get; set; }
+        public Dictionary<string, string> Actors { get; set; }
+		public string[] Genre { get; set; }
+		public string Type { get; set; }
 		public double Rate { get; set; }
 		public double Views { get; set; }
 		public string Image { get; set; }
